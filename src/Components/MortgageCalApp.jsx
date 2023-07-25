edd0df5e9f54fc2dabb4681a26c5d6b5cacd2c37
zarman imran <Zarman-oss>
2023-07-25 10:53:28 +0500

    third commit

--- src/Components/MortgageCalApp.jsx
+++ src/Components/MortgageCalApp.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import Header from './Header';
 
 function MortgageCalApp() {
   return (
-    <div className='container mt-4 card'>MortgageCalApp</div>
-  )
+    <div className="container mt-4 card" style={{ width: 500 }}>
+        <div className='container-1'>
+        <Header text={'Mortgage Calculator For Your Home'} />
+        </div>
+     
+    </div>
+  );
 }
 
-export default MortgageCalApp
+export default MortgageCalApp;
